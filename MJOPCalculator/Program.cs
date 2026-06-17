5674b32f3a29b5441e8c55e67b42bc79a006ff18
Lars Lis <llissenberg@kvt.nl>
2026-06-17 14:09:32 +0200

    deployment

--- MJOPCalculator/Program.cs
+++ MJOPCalculator/Program.cs
@@ -13,13 +13,19 @@ builder.Services.AddSingleton<EquipmentModelService>();
 
 var app = builder.Build();
 
+var pathBase = builder.Configuration["ASPNETCORE_PATHBASE"];
+if (!string.IsNullOrWhiteSpace(pathBase))
+{
+    app.UsePathBase(pathBase);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
